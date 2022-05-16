7dfba11e4eb033bc1b05383647b13f5d1540215b
chenzhian <chen_zhian@139.com>
2022-05-16 21:49:29 +0800

    增加tutorial模块

---
+++ awekit-tutorial/src/main/java/com/simiam/awekit/tutorial/hadoop/HadoopLauncher.java
@@ -0,0 +1,11 @@
+package com.simiam.awekit.tutorial.hadoop;
+
+/**
+ * <p>Title: HadoopLauncher</p>
+ * <p>Description:</p>
+ * <p>Copyright: FJ.CMCC Co., Ltd. (c) 2022</p>
+ * <p>@Author: chenzhian </p>
+ * <p>@Date: 2022/5/16 21:46</p>
+ */
+public class HadoopLauncher {
+}
---
+++ awekit-tutorial/src/test/java/com/simiam/awekit/tutorial/HadoopTest.java
@@ -0,0 +1,11 @@
+package com.simiam.awekit.tutorial;
+
+/**
+ * <p>Title: HadoopTest</p>
+ * <p>Description:</p>
+ * <p>Copyright: FJ.CMCC Co., Ltd. (c) 2022</p>
+ * <p>@Author: chenzhian </p>
+ * <p>@Date: 2022/5/16 21:48</p>
+ */
+public class HadoopTest {
+}
